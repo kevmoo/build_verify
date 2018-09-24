ff9698d3650ac6f9d10fd744cf591839fe7d6321
Kevin Moore <kevmoo@users.noreply.github.com>
2018-09-24 09:37:50 -0700

    First functioning commit (#2)

---
+++ README.md
@@ -0,0 +1,19 @@
+Test utility to ensure generated Dart code within a package is up-to-date
+when using `package:build`.
+
+### Usage
+
+Assuming your package is already configured to use `package:build_runner` –
+`pub run build_runner build` succeeds – then simply add a unit test to the
+test directory.
+
+For example: `test/ensure_build_test.dart` containing:
+
+```dart
+import 'package:build_verify/build_verify.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('ensure_build', expectBuildClean);
+}
+```
---
+++ lib/src/utils.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+
+/// The path to the root directory of the SDK.
+final String _sdkDir = (() {
+  // The Dart executable is in "/path/to/sdk/bin/dart", so two levels up is
+  // "/path/to/sdk".
+  var aboveExecutable = p.dirname(p.dirname(Platform.resolvedExecutable));
+  assert(FileSystemEntity.isFileSync(p.join(aboveExecutable, 'version')));
+  return aboveExecutable;
+})();
+
+final String pubPath =
+    p.join(_sdkDir, 'bin', Platform.isWindows ? 'pub.bat' : 'pub');
