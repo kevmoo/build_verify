2350b0266d44225ae1ef0c5b89e4c6d1ad21f10f
Kevin Moore <kevmoo@google.com>
2025-12-07 16:35:38 -0800

    Latest deps, bump min SDK

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -3,20 +3,19 @@ import 'dart:io';
 import 'package:path/path.dart' as p;
 
 /// The path to the root directory of the SDK.
-final String dartPath =
-    (() {
-      final executableBaseName = p.basename(Platform.resolvedExecutable);
+final String dartPath = (() {
+  final executableBaseName = p.basename(Platform.resolvedExecutable);
 
-      if (executableBaseName == 'flutter_tester' ||
-          executableBaseName == 'flutter_tester.exe') {
-        final flutterRoot = Platform.environment['FLUTTER_ROOT']!;
+  if (executableBaseName == 'flutter_tester' ||
+      executableBaseName == 'flutter_tester.exe') {
+    final flutterRoot = Platform.environment['FLUTTER_ROOT']!;
 
-        return p.join(flutterRoot, 'bin', 'cache', 'dart-sdk', 'bin', 'dart');
-      } else {
-        assert(
-          executableBaseName == 'dart' || executableBaseName == 'dart.exe',
-          'Was not expected "$executableBaseName".',
-        );
-        return Platform.resolvedExecutable;
-      }
-    })();
+    return p.join(flutterRoot, 'bin', 'cache', 'dart-sdk', 'bin', 'dart');
+  } else {
+    assert(
+      executableBaseName == 'dart' || executableBaseName == 'dart.exe',
+      'Was not expected "$executableBaseName".',
+    );
+    return Platform.resolvedExecutable;
+  }
+})();
